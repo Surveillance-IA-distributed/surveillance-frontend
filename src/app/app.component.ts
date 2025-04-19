91ebdedeede9a02f739be2b6f4fe2f82fa0a559e
Angel <angelloayzah2016@gmail.com>
2025-04-19 03:07:51 -0500

    feat: implement layout with Sidebar and Topbar components in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './nav/sidebar/sidebar.component';
+import { TopbarComponent } from './nav/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    TopbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  isSideBarOpen = true;
+
+  toggleSidebar( isOpen: boolean) {
+    console.log('PADRE: toggleSidebar() ->', isOpen);
+    this.isSideBarOpen = isOpen;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { ScanVideoComponent } from './scan-video/scan-video.component';
+import { CreateRequestComponent } from './create-request/create-request.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +12,17 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
-    }
+    },
+    {
+        path: 'upload-video',
+        component: UploadVideoComponent
+    },
+    {
+        path: 'scan-video',
+        component: ScanVideoComponent 
+    },
+    {
+        path: 'create-request',
+        component: CreateRequestComponent
+    },
 ];
