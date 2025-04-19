f3a7dd17c93a57667b21e136699ab9d7c37d60a4
Angel <angelloayzah2016@gmail.com>
2025-04-18 23:15:13 -0500

    feat: create HomeComponent with routing, template, styles, and tests

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
