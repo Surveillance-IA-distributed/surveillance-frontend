0882ae6a114a40c010de2e4bd790c543f83cd0ed
Angel <angelloayzah2016@gmail.com>
2025-05-07 03:25:07 -0500

    feat: Add MakeQueryResponse and RequestBodySend interfaces for request handling

---
+++ src/app/create-request/interfaces/makeQueryResponse.interface.ts
@@ -0,0 +1,5 @@
+export interface MakeQueryResponse {
+    video_name:     string;
+    sec?:           number;
+    object_count?:  number;
+}
---
+++ src/app/create-request/interfaces/requestBodySend.interface.ts
@@ -0,0 +1,8 @@
+export interface RequestBodySend {
+    type: string;
+    video_name: string;
+    environment_type: string | null;
+    object_name: string | null;
+    color: string | null;
+    proximity: string | null;
+}
