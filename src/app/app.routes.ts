62db7155cf2bdde216d636f69166bcc1b5a41db6
Angel <angelloayzah2016@gmail.com>
2025-06-11 03:25:18 -0500

    feat: Comment out unused route and sidebar items for video processing features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
-import { ScanVideoComponent } from './scan-video/scan-video.component';
-import { CreateRequestComponent } from './create-request/create-request.component';
-import { CreateAlertComponent } from './create-alert/create-alert.component';
-import { ResultsVideosComponent } from './results-videos/results-videos.component';
+// import { ScanVideoComponent } from './scan-video/scan-video.component';
+// import { CreateRequestComponent } from './create-request/create-request.component';
+// import { CreateAlertComponent } from './create-alert/create-alert.component';
+// import { ResultsVideosComponent } from './results-videos/results-videos.component';
 
 export const routes: Routes = [
     {
@@ -19,20 +19,20 @@ export const routes: Routes = [
         path: 'upload-video',
         component: UploadVideoComponent
     },
-    {
-        path: 'scan-video',
-        component: ScanVideoComponent 
-    },
-    {
-        path: 'create-request',
-        component: CreateRequestComponent
-    },
-    {
-        path: 'create-alert',
-        component: CreateAlertComponent
-    },
-    {
-        path: 'results-videos',
-        component: ResultsVideosComponent
-    }
+    // {
+    //     path: 'scan-video',
+    //     component: ScanVideoComponent 
+    // },
+    // {
+    //     path: 'create-request',
+    //     component: CreateRequestComponent
+    // },
+    // {
+    //     path: 'create-alert',
+    //     component: CreateAlertComponent
+    // },
+    // {
+    //     path: 'results-videos',
+    //     component: ResultsVideosComponent
+    // }
 ];
